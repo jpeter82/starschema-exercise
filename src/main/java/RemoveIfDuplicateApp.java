2a376d8507f4077819fcebfe85d129f857210100
jpeter82 <jpeter82@gmail.com>
2018-02-26 10:21:07 +0100

    Solution: using only primitives

--- src/main/java/RemoveIfDuplicateApp.java
+++ src/main/java/RemoveIfDuplicateApp.java
@@ -1,24 +1,44 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class RemoveIfDuplicateApp {
 
     public static void main(String[] args) {
-        List<Integer> originalList = Arrays.asList(0, 1, 2, 3, 0, 0, 2);
+        int[] originalArray = {0, 1, 2, 3, 0, 0, 2};
 
         RemoveIfDuplicateApp app = new RemoveIfDuplicateApp();
-        List<Integer> listWithoutDuplicates = app.removeAllDuplicates(originalList);
+        int[] listWithoutDuplicates = app.removeAllDuplicates(originalArray);
 
-        System.out.println("Original List: " + originalList.toString());
+        System.out.println("Original List: " + Arrays.toString(originalArray));
 
-        System.out.println("Output List: " + listWithoutDuplicates.toString());
+        System.out.println("Output List: " + Arrays.toString(listWithoutDuplicates));
     }
 
-    public List<Integer> removeAllDuplicates(List<Integer> originalList) {
-        //TODO: implement this method...
+    public int[] removeAllDuplicates(int[] originalArray) {
+        Arrays.sort(originalArray);
+        int[] outputArray = {};
+        int counter = 0;
 
-        return new ArrayList<>();
+        for (int i = 0 ; i < originalArray.length - 1; i++) {
+            if ((originalArray[i] != originalArray[i + 1]) && counter == 0) {
+                outputArray = addToArray(originalArray[i], outputArray);
+            } else if ((originalArray[i] != originalArray[i + 1]) && (counter != 0)) {
+                counter = 0;
+            } else {
+                counter++;
+            }
+        }
+
+        if (originalArray[originalArray.length - 2] != originalArray[originalArray.length - 1]) {
+            outputArray = addToArray(originalArray[originalArray.length - 1], outputArray);
+        }
+
+        return outputArray;
+    }
+
+    public int[] addToArray(int element, int[] array) {
+        array = Arrays.copyOf(array, array.length + 1);
+        array[array.length - 1] = element;
+        return array;
     }
 
 }
