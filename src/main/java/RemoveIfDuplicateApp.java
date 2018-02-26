260735b9e185597673eedb6e0e5d7abd0d1cc85e
jpeter82 <jpeter82@gmail.com>
2018-02-26 14:56:50 +0100

    Solution: preserving order, using primitives (inside algorithm)

--- src/main/java/RemoveIfDuplicateApp.java
+++ src/main/java/RemoveIfDuplicateApp.java
@@ -5,20 +5,47 @@ import java.util.List;
 public class RemoveIfDuplicateApp {
 
     public static void main(String[] args) {
-        List<Integer> originalList = Arrays.asList(0, 1, 2, 3, 0, 0, 2);
+        int[] originalArray = {0, 1, 2, 3, 0, 0, 2};
 
         RemoveIfDuplicateApp app = new RemoveIfDuplicateApp();
-        List<Integer> listWithoutDuplicates = app.removeAllDuplicates(originalList);
+        List<Integer> listWithoutDuplicates = app.removeAllDuplicates(originalArray);
 
-        System.out.println("Original List: " + originalList.toString());
+        System.out.println("Original List: " + Arrays.toString(originalArray));
 
         System.out.println("Output List: " + listWithoutDuplicates.toString());
     }
 
-    public List<Integer> removeAllDuplicates(List<Integer> originalList) {
-        //TODO: implement this method...
+    public List<Integer> removeAllDuplicates(int[] originalArray) {
+        List<Integer> outputList = new ArrayList<>();
 
-        return new ArrayList<>();
+        if (originalArray.length < 2) {
+            for (int i : originalArray) {
+                outputList.add(i);
+            }
+        } else {
+            boolean inValidated = false;
+            boolean[] inValidHelper = new boolean[originalArray.length];
+
+            for (int i = 0; i < originalArray.length - 1; i++) {
+                if (!inValidHelper[i]){
+                    for (int j = i + 1; j < originalArray.length; j++) {
+
+                        if (originalArray[i] == originalArray[j]) {
+                            inValidHelper[j] = true;
+                            inValidated = true;
+                        }
+                    }
+
+                    if (!inValidated) {
+                        outputList.add(originalArray[i]);
+                    }
+
+                    inValidated = false;
+                }
+            }
+        }
+
+        return outputList;
     }
 
 }
